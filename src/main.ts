6f2cbf6ae571712cd7f3fa955b34a31d12127c66
Iniobong Ukpong <iniobong10000@gmail.com>
2026-06-21 22:43:35 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,18 @@ async function bootstrap() {
 
   // Enable CORS with specific configuration
   app.enableCors({
-    origin: ['https://rccg-jcc.vercel.app', 'https://jcc-admin.vercel.app', 'http://localhost:3000', 'http://localhost:3001', 'https://www.experiencejubilee.org'], // Allow requests from your frontend
+    origin: [
+      'https://rccg-jcc.vercel.app',
+      'https://jcc-admin.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://www.experiencejubilee.org',
+      'https://experiencejubilee.org',
+    ], // Allow requests from your frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed methods
     credentials: true, // Allow cookies if needed
   });
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
